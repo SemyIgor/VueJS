cab8587c62b4091b0edf79d241038c583e5a2dc6
HenkSeed <adaner@inbox.ru>
2023-09-13 00:03:41 +0700

    Added changeing color of the price

--- Lesson10_HW/script.js
+++ Lesson10_HW/script.js
@@ -5,7 +5,7 @@ Vue.component('product-details', {
 			product: {
 				name: 'Boots',
 				price: 8200,
-				numberProduct: 5,
+				numberProduct: 0,
 			},
 		};
 	},
@@ -20,11 +20,18 @@ Vue.component('product-details', {
 				return false;
 			}
 		},
+		textColor() {
+			if (this.product.numberProduct > 0) {
+				return 'green';
+			} else {
+				return 'red';
+			}
+		},
 	},
 	template: `
    <div>
       <h1>Название продукта: {{product.name}}</h1>
-      <h1>Цена продукта: {{formattedPrice}}</h1>
+      <h1>Цена продукта: <span :style="{color: textColor}">{{formattedPrice}}</span></h1>
       <h1 v-if = "available" class="green">Продукт доступен</h1>
       <h1 v-else class="red">Продукта нет на складе</h1>
    </div>
